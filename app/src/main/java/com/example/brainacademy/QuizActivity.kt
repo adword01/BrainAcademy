8961698e24562618fe85923dc212b96feae1d623
Aditya <21304@iiitu.ac.in>
2023-03-17 09:00:57 +0530

    Initial Commit

--- app/src/main/java/com/example/brainacademy/QuizActivity.kt
+++ app/src/main/java/com/example/brainacademy/QuizActivity.kt
@@ -1,196 +1,152 @@
 package com.example.brainacademy
 
 
-import android.content.Intent
+import QuizAdapter
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.brainacademy.databinding.ActivityQuizBinding
 import com.google.firebase.database.*
-//import kotlinx.coroutines.NonCancellable.message
+
 
 class QuizActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityQuizBinding
+    private lateinit var optionsAdapter: QuizAdapter
+    private lateinit var questionsList: MutableList<questiondatabse>
+    private var currentQuestionIndex = 0
+    private var score = 0
 
-    private var askedQuestions = mutableListOf<String>()
-    var flag: Boolean = true
+    private lateinit var selectedCategory: String
 
     private lateinit var database: DatabaseReference
-    private lateinit var selected_category: String
-    private var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        binding = ActivityQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Get the selected category from the intent
+      //  selectedCategory = intent.getStringExtra("CATEGORY").toString()
 
-        // val nextButton = findViewById<Button>(R.id.nextButton)
-        val category = findViewById<TextView>(R.id.category)
-
-
-        val intent = intent
-        selected_category = intent.getStringExtra("category").toString().trim()
-        var length = intent.getIntExtra("length",0)
-
-
-        category.setText(selected_category)
-
-        displayQuestions()
-
-    }
-
-    private fun displayQuestions() {
+        // Initialize Firebase database
         database = FirebaseDatabase.getInstance().reference.child("Questions")
-        val questionTextView = findViewById<TextView>(R.id.questionTextView)
-        val option1TextView = findViewById<TextView>(R.id.option1)
-        val option2TextView = findViewById<TextView>(R.id.option2)
-        val option3TextView = findViewById<TextView>(R.id.option3)
-        val option4TextView = findViewById<TextView>(R.id.option4)
-        val subcategoryTextView = findViewById<TextView>(R.id.subcategory)
 
-        // Retrieve questions from all subcategories under "Science"
-        database.child("Science").addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val questionList = mutableListOf<questiondatabse>()
-                for (subCategorySnapshot in snapshot.children) {
-                    for (questionSnapshot in subCategorySnapshot.children) {
-                        val questionDescription = questionSnapshot.child("description").getValue(String::class.java)
-                        val option1 = questionSnapshot.child("option1").getValue(String::class.java)
-                        val option2 = questionSnapshot.child("option2").getValue(String::class.java)
-                        val option3 = questionSnapshot.child("option3").getValue(String::class.java)
-                        val option4 = questionSnapshot.child("option4").getValue(String::class.java)
-                        val subcategory = questionSnapshot.child("subcategory").getValue(String::class.java)
-                        val correctAnswer = questionSnapshot.child("correctanswer").getValue(String::class.java)
+        // Set up the RecyclerView and adapter
+        questionsList = mutableListOf()
+        optionsAdapter = QuizAdapter(this, questionsList, R.color.selected_option)
 
-                        val question = questiondatabse(
-                            questionDescription!!,
-                            option1!!,
-                            option2!!,
-                            option3!!,
-                            option4!!,
-                            subcategory!!,
-                            correctAnswer!!
-                        )
-                        questionList.add(question)
-                    }
-                }
+        binding.Quizrecylerview.adapter = optionsAdapter
+        binding.Quizrecylerview.layoutManager = LinearLayoutManager(this)
 
-                // Select a random question that hasn't been asked yet
-                val unansweredQuestions = questionList.filter { it.description !in askedQuestions }
+        // Retrieve the questions for the selected category from Firebase database
+        database.child("Science")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (subCategorySnapshot in snapshot.children) {
+                        for (questionSnapshot in subCategorySnapshot.children) {
+                            val questionDescription =
+                                questionSnapshot.child("description").getValue(String::class.java)
+                            val option1 =
+                                questionSnapshot.child("option1").getValue(String::class.java)
+                            val option2 =
+                                questionSnapshot.child("option2").getValue(String::class.java)
+                            val option3 =
+                                questionSnapshot.child("option3").getValue(String::class.java)
+                            val option4 =
+                                questionSnapshot.child("option4").getValue(String::class.java)
+                            val correctAnswer =
+                                questionSnapshot.child("correctanswer").getValue(String::class.java)
+                            val subcategory =
+                                questionSnapshot.child("subcategory").getValue(String::class.java)
 
-                if (unansweredQuestions.isNotEmpty()) {
-                    val randomQuestion = unansweredQuestions.random()
-                    questionTextView.text = randomQuestion.description
-                    option1TextView.text = randomQuestion.option1
-                    option2TextView.text = randomQuestion.option2
-                    option3TextView.text = randomQuestion.option3
-                    option4TextView.text = randomQuestion.option4
-                    subcategoryTextView.text = randomQuestion.subcategory
-                    // Add the question description to the list of asked questions
-                    askedQuestions.add(randomQuestion.description)
-
-                    // Define selectedOption at the class level
-                    var selectedOption: String? = null
-
-// Add an onClickListener to each of the option TextViews
-                    option1TextView.setOnClickListener {
-                        selectedOption = option1TextView.text.toString()
-                        Toast.makeText(this@QuizActivity, "$selectedOption", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                    option2TextView.setOnClickListener {
-                        selectedOption = option2TextView.text.toString()
-                        Toast.makeText(this@QuizActivity, "$selectedOption", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                    option3TextView.setOnClickListener {
-                        selectedOption = option3TextView.text.toString()
-                        Toast.makeText(this@QuizActivity, "$selectedOption", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                    option4TextView.setOnClickListener {
-                        selectedOption = option4TextView.text.toString()
-                        Toast.makeText(this@QuizActivity, "$selectedOption", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-
-// Add an onClickListener to the nextButton
-                    val nextButton = findViewById<Button>(R.id.nextButton)
-//                    val index = 0
-                    nextButton.setOnClickListener {
-                        // Check the selected answer and update the score
-                        checkAnswer(selectedOption.toString(), randomQuestion.correctanswer)
-                        val length = intent.getIntExtra("length",0)
-
-                        // Clear the selected option variable
-//                    selectedOption = null
-                        // Display the next question or show the final score
-                        if (askedQuestions.size <= (length-1)) {
-//                            askedQuestions.size++
-//                            Toast.makeText(
-//                                this@QuizActivity,
-//                                "${questionList.size}",
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-                            displayQuestions()
+                            val question = questiondatabse(
+                                questionDescription!!,
+                                option1!!,
+                                option2!!,
+                                option3!!,
+                                option4!!,
+                                correctAnswer!!,
+                                subcategory!!
+                            )
+                            questionsList.add(question)
                         }
-//                    if (askedQuestions.size >= questionList.size) {
-//                      //  showFinalScore()
-//                    }
-//                    else {
-//                        Toast.makeText(this@QuizActivity,"SCore",Toast.LENGTH_SHORT).show()
-////                        showFinalScore()
-//                    }
-
                     }
-
-                } else {
-                    if (flag) {
-                        flag = false
-                        showFinalScore()
-
-                    }
-//                ++flag
-//             //   Toast.makeText(this@QuizActivity,"Toast",Toast.LENGTH_SHORT).show()
-//                if(flag == 1){
-
-//                    showFinalScore()
-//                }
-                    // All questions have been asked, exit the activity
-                    //   val intent = Intent(this@QuizActivity,ResultActivity::class.java)
-                    finish()
-
+                    optionsAdapter.notifyDataSetChanged()
+                    showNextQuestion()
                 }
 
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error
+                }
+            })
+
+        // Set up the click listener for the "Next" button
+        // Set up the click listener for the "Next" button
+        binding.nextButton.setOnClickListener {
+            val selectedOption = optionsAdapter.getSelectedOption(currentQuestionIndex)
+            if (selectedOption == null) {
+                Toast.makeText(this, "Please select an option", Toast.LENGTH_SHORT).show()
+            } else {
+                if (selectedOption == questionsList[currentQuestionIndex].correctanswer) {
+                    score++
+                }
+                currentQuestionIndex++
+                if (currentQuestionIndex < questionsList.size) {
+                    showNextQuestion()
+                } else {
+                    showQuizResult()
+                }
             }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(
-                    this@QuizActivity,
-                    "Failed to retrieve questions for $selected_category",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        })
-
-
-    }
-
-
-    // Function to check if the selected option is correct and update the score
-    private fun checkAnswer(selectedOption: String, correctAnswer: String?) {
-
-        if (selectedOption.toString().trim() == correctAnswer.toString().trim()) {
-            score++
         }
-        displayQuestions()
+
+        // Function to show the next question
+
+
+        // Function to show the quiz result
+
+    }
+    private fun showQuizResult() {
+        val resultPercentage = (score.toFloat() / questionsList.size.toFloat()) * 100
+        Toast.makeText(this,"$resultPercentage",Toast.LENGTH_SHORT).show()
+//        val resultMessage =
+//            getString(R.string.result_message, score, questionsList.size, resultPercentage)
+       // val dialog = QuizResultDialog(this, resultMessage)
+       // dialog.show()
     }
 
-    fun showFinalScore() {
-//        val message = "Your final score is $score "
-        Toast.makeText(this@QuizActivity, "$score", Toast.LENGTH_LONG).show()
+    private fun showNextQuestion() {
+        // Check if we have reached the end of the questions list
+        if (currentQuestionIndex == questionsList.size) {
+            // Show the final score and end the activity
+            Toast.makeText(this, "Your final score is $score", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
 
-        // You can also start a new activity to show the final score in a different screen
+        // Set the question description and options in the UI
+        val currentQuestion = questionsList[currentQuestionIndex]
+        binding.questionTextView.text = currentQuestion.description
+        currentQuestion.useranswer?.let { userAnswer ->
+            optionsAdapter.setSelectedOption(currentQuestionIndex,userAnswer)
+        }
+
+        // Update the question index and reset the user answer
+        currentQuestionIndex++
+        currentQuestion.useranswer = ""
     }
+
+//    private fun showNextQuestion() {
+//        val question = questionsList[currentQuestionIndex]
+//        binding.questionTextView.text = question.description
+//        optionsAdapter.setOptions(
+//            question.option1,
+//            question.option2,
+//            question.option3,
+//            question.option4
+//        )
+//        binding.questionCountTextView.text =
+//            getString(R.string.question_count, currentQuestionIndex + 1, questionsList.size)
+//    }
 }
--- app/src/main/java/com/example/brainacademy/questiondatabse.kt
+++ app/src/main/java/com/example/brainacademy/questiondatabse.kt
@@ -1,9 +1,21 @@
 package com.example.brainacademy
+class questiondatabse(
+    var description: String = "",
+    var option1: String = "",
+    var option2: String = "",
+    var option3: String = "",
+    var option4: String = "",
+    var correctanswer: String = "",
+    var useranswer: String = ""
+) {
 
-data class questiondatabse(val description: String = "",
-                           var option1 : String = "",
-                           var option2 : String = "",
-                           var option3 : String = "",
-                           var option4 : String = "",
-                           var subcategory : String = "",
-                           var correctanswer : String = "")
+    fun getOptionAt(position: Int): String {
+        return when (position) {
+            0 -> option1
+            1 -> option2
+            2 -> option3
+            3 -> option4
+            else -> ""
+        }
+    }
+}
